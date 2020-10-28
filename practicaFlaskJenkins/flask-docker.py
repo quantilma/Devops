fb047610f084270a0e2ca61c3843e2921c2e17b2
julian <juliannino00@gmail.com>
2020-10-28 19:36:40 +0000

    Actualizar flask-docker.py

--- practicaFlaskJenkins/flask-docker.py
+++ practicaFlaskJenkins/flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Buenas Tardes Buenas Tardes Buenas Noches------ JULIAN'
+    return 'Buenas Tardes Buenas Tardes Buenas Noches------ JULIAN NINOOOOO'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
