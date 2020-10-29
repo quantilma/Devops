7150ea8415caa45d774933de23087e9dea27fc82
julianNinoo <julian.nino@correo.icesi.edu.co>
2020-10-29 21:11:39 +0000

    Melos

--- practicaFlaskJenkins/flask-docker.py
+++ practicaFlaskJenkins/flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'anadido el webook'
+    return 'anadido el webook -asa'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
