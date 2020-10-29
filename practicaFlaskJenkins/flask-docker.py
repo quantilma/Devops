96c996a6709d1658d918e227de3eb6779668247e
julian <juliannino00@gmail.com>
2020-10-29 05:25:50 +0000

    si buenas

--- practicaFlaskJenkins/flask-docker.py
+++ practicaFlaskJenkins/flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Escriba su nombre aqui re malparidooo'
+    return 'Escriba su nombre aqui señoro'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
