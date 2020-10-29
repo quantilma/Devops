79ec265cc1741abeafffb7c4cc08931f87acfe21
julianNinoo <julian.nino@correo.icesi.edu.co>
2020-10-29 19:19:14 +0000

    buenas tardes

--- practicaFlaskJenkins/flask-docker.py
+++ practicaFlaskJenkins/flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'entoncesssss  entoncesssss    Buenas Tardes Buenas Tardes Buenas Noches------ JULIAN NINOOOO-------------- sdfs sdfsdd-O'
+    return 'entonces me da o no'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
