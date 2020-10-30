2c8a377a0736df23059a23e3723ebc0f7319edb4
julianNinoo <julian.nino@correo.icesi.edu.co>
2020-10-30 14:11:43 -0500

    update flask
    hola

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'garcia es un re malparido'
+    return 'Por favor escriba su nombre aqui:'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
