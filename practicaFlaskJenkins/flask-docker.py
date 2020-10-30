212b53764e24a5f90c0114f54a7ac5da14db4583
root <root@maquinadevops.us-central1-a.c.practicadevops.internal>
2020-10-30 15:50:07 +0000

    Cambio Final

--- practicaFlaskJenkins/flask-docker.py
+++ practicaFlaskJenkins/flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'anadido el webook holaa2'
+    return 'ultima o q'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
