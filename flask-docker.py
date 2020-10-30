f7b725587926555e091e7fcb088ad732e455f710
root <root@maquinadevops.us-central1-a.c.practicadevops.internal>
2020-10-30 16:23:30 +0000

    vamos que vamos

--- flask-docker.py
+++ flask-docker.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'felipe es un gran marica'
+    return 'garcia es un re malparido'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
