c4cd5cf9a612cea45d3d431f7706aef091a953b8
root <root@maquinadevops.us-central1-a.c.practicadevops.internal>
2020-11-03 03:29:17 +0000

    Cambio7 02 de Noviembre

--- practicaFlaskJenkins/hello.py
+++ practicaFlaskJenkins/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Buenas noches 02/11/2020'
+    return 'Buenas noches hoy 02/11/2020'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
