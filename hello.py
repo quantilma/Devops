6c3d5f6c3b17000abb6608c399d09f3a4f427930
julianNinoo <julian.nino@correo.icesi.edu.co>
2020-11-05 09:54:58 -0500

    actualizar nuevo archivo
    nuevo archivo

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Buenos días 05/11/2020'
+    return 'Buenos días  Investigador1'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
