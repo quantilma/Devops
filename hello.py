eb4c4ff8aac51d89a18a94716019472e4c8e7369
julianNinoo <julian.nino@correo.icesi.edu.co>
2020-11-05 09:37:04 -0500

    Update archivo
    se cambio el archivo

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Buenas tardes 03/11/2020'
+    return 'Buenos días 05/11/2020'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
