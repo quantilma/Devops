67ed3bb8fe5c2fb34c887c0364ea346465af08dd
JulianEChitiva <julianechitiva@iMac-de-Julian.local>
2020-11-13 12:32:11 -0500

    Quitar tildes
    Se quito los caracteres no ASCII

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Buenos días. Hoy es 13-11-2020.'
+    return 'Buenos dias. Hoy es 13-11-2020.'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0')
